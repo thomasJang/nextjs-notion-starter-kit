72663098b7ce36bbd235fa7631848731b4e7fa29
Thomas <tom@axisj.com>
2021-07-22 20:05:10 +0900

    Fix favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,18 +8,10 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.png' />
-
             <link
               rel='apple-touch-icon'
               sizes='180x180'
               href='/apple-touch-icon.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='96x96'
-              href='/favicon-96x96.png'
             />
             <link
               rel='icon'
@@ -33,8 +25,14 @@ export default class MyDocument extends Document {
               sizes='16x16'
               href='/favicon-16x16.png'
             />
-
-            <link rel='manifest' href='/manifest.json' />
+            <link rel='manifest' href='/site.webmanifest' />
+            <link
+              rel='mask-icon'
+              href='/safari-pinned-tab.svg'
+              color='#5bbad5'
+            />
+            <meta name='msapplication-TileColor' content='#da532c' />
+            <meta name='theme-color' content='#ffffff' />
           </Head>
 
           <body>
